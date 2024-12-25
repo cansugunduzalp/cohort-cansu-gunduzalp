90d0032887c1c6b248f388553e41692cc178ca46
user <user@DESKTOP-GVHF1Q9>
2024-12-25 13:08:12 +0300

    Add project files.

---
+++ cohort cansu gunduzalp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string ad = "Cansu"; // Adınızı buraya yazın
+        string soyad = "Gündüzalp Süner"; // Soyadınızı buraya yazın
+        string neden = "Yazılım geliştirme konusunda kendimi geliştirmek ve yeni teknolojiler öğrenmek istiyorum.";
+       
+
+        Console.WriteLine("Ad: " + ad);
+        Console.WriteLine("Soyad: " + soyad);
+        Console.WriteLine("Patika Bootcamp'inde olma nedenim: " + neden);
+
+        
+        Console.ReadLine();
+
+
+    }
+}
